7ef795f3608401b0d38023a9f77ac1dd60127025
Jesus Adonis <jar285@njit.edu>
2024-06-09 23:01:35 -0400

    Added analytics and speedinsigths to the layout, modify navbar, and added some images that might help ,ater

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import Navbar from "@/components/main/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +23,9 @@ export default function RootLayout({
       <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
         <StarsCanvas />
+        <Navbar/>
+        <SpeedInsights />
+        <Analytics/>
         {children}
         </body>
     </html>
